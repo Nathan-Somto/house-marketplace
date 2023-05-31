d7601f025dd7fedafc7d97e9adbcf41c80a93723
Mkparu <mkparusomtochi26@gmail.com>
2023-05-31 19:29:46 +0100

    Implemented password reset

--- pages/forgot-password/index.tsx
+++ pages/forgot-password/index.tsx
@@ -1,33 +1,35 @@
-import Image from 'next/image'
-import Link from 'next/link';
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { auth } from "@/firebase/firebase.config";
+import { sendPasswordResetEmail } from "firebase/auth";
+import Image from "next/image";
+import Link from "next/link";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 function forgotPasswordPage() {
-    const [email,setEmail] = useState('');
-    function handleChange (e:ChangeEvent<HTMLInputElement>){
-            setEmail(e.target.value);
+  const [email, setEmail] = useState("");
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+  }
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if(email === '') return;
+    try {
+      await sendPasswordResetEmail(auth, email);
+      toast.success(`we just sent an email to ${email}`);
+    } catch (err) {
+      toast.error("there was an error while sending the email.");
     }
-    function handleSubmit(e: FormEvent<HTMLFormElement>){
-        e.preventDefault();
-        try{
-            console.log("sent reset email");
-        }
-        catch(err){
-
-        }
-    }
+  }
   return (
     <main className="w-full bg-primary-grey min-h-screen relative">
-            <Link href="/" className='absolute top-[30px] left-[30px] fill-[#fff]'>       
-            <Image
-              src="/svg/homeIcon.svg"
-              alt="home icon"
-              width={30}
-              height={30}
-            />
-            </Link>
-            <form onSubmit={handleSubmit} className="text-left justify-center h-screen space-y-6 px-6 lg:px-0 max-w-[600px] mx-auto flex flex-col items-center">
-            <h1 className='w-full'>Forgot Password</h1>
-            <div className="w-full">
+      <Link href="/" className="absolute top-[30px] left-[30px] fill-[#fff]">
+        <Image src="/svg/homeIcon.svg" alt="home icon" width={30} height={30} />
+      </Link>
+      <form
+        onSubmit={handleSubmit}
+        className="text-left justify-center h-screen space-y-6 px-6 lg:px-0 max-w-[600px] mx-auto flex flex-col items-center"
+      >
+        <h1 className="w-full">Forgot Password</h1>
+        <div className="w-full">
           <label htmlFor="email" className="font-medium text-gray-600">
             Email
           </label>
@@ -50,9 +52,16 @@ function forgotPasswordPage() {
             />
           </div>
         </div>
-        <Link href="/signin" className='text-right font-semibold text-primary-green w-full'>Sign in</Link>
+        <Link
+          href="/signin"
+          className="text-right font-semibold text-primary-green w-full"
+        >
+          Sign in
+        </Link>
         <div className="flex justify-between w-full items-center">
-          <h4 className="text-2xl font-medium text-gray-600">Send Reset Email</h4>
+          <h4 className="text-2xl font-medium text-gray-600">
+            Send Reset Email
+          </h4>
           <button className="relative h-[50px] w-[50px] flex hover:opacity-50 duration-200 ease-in items-center justify-center rounded-full shadow-lg bg-primary-green p-2">
             <Image
               src="/svg/keyboardArrowRightIcon.svg"
@@ -62,10 +71,9 @@ function forgotPasswordPage() {
             />
           </button>
         </div>
-        </form>
+      </form>
     </main>
-   
-  )
+  );
 }
 
-export default forgotPasswordPage
+export default forgotPasswordPage;
